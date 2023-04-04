57a998f3dad82b1fa63f6fb70a8435ced9b499c1
Samuel <samnmbuguah@gmail.com>
2023-04-04 05:36:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const sums = array.map((number, index) => array.slice(index + 1).map(secondNumber => number + secondNumber));
+  console.log(sums);
+  const concatenatedSums = [].concat(...sums);
+  console.log(concatenatedSums);
+  return concatenatedSums.includes(target);
 }
 
 /* 
-  Write the Big O time complexity of your function here
+The big O time complexity of my function is  O(n²) because there is a nested iteration consisting of a map method inside another map method.
 */
 
 /* 
-  Add your pseudocode here
+Initialize a variable to hold arrays of all possible sums in the input array.
+Iterate over the array twice getting all possible sums.
+Concatenate the arrays to create one array of all possible sums.
+Check if the sums include the target sum. 
 */
 
 /*
-  Add written explanation of your solution here
+  I initialized an array sum to take up arrays of sums for each number in the array with the other numbers and assigned it to an expression.
+  In the expression i used map to iterate over the array, passed a number and its' index as the arguments for the callback function.
+  I used slice to create a new array starting from the next index of the number being currently iterated. 
+  I used another map to iterate over the new array and return the second number to add to the first number being iterated over.
+  In the second maps' callback function i added the first number to the second number.
+  Sums was now equal to an array of all possible sums of the numbers in the array  passed, console logged to verify this.
+  I initialized a new variable concatenatedSums and set it to an empty array.
+  I used the .concat method to concatenate all the items in the arrays in sums using a spread operator into the new array.
+  I used the .includes method to check if the target sum passed in as a parameter to the function hasTargetSum is included in the concatenated array and returned the boolean result.
 */
 
 // You can run `node index.js` to view these console logs
@@ -27,8 +42,18 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([5, 19, -4, 6, 30], 26));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
